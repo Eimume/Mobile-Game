5551ea5097bac470b2c6c72e2bd66c490a1343ff
Eimume <168836948+Eimume@users.noreply.github.com>
2024-09-23 19:19:44 +0700

    strawberry fix

---
+++ Assets/Code/Weapon.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Weapon")]
+public class Weapon : ScriptableObject
+{
+    public string weaponName;
+    public int damage;
+    public float attackSpeed;
+    public Sprite weaponIcon;
+    public GameObject weaponPrefab;
+
+    // ฟังก์ชันโจมตีสำหรับดาบหรือปืน
+    public virtual void Attack()
+    {
+        Debug.Log("Attacking with " + weaponName);
+    }
+}
+
+[CreateAssetMenu(fileName = "NewGun", menuName = "Weapons/Gun")]
+public class Gun : Weapon
+{
+    public int ammo;  // จำนวนกระสุนของปืน
+
+    // ฟังก์ชันโจมตีเฉพาะสำหรับปืน
+    public override void Attack()
+    {
+        if (ammo > 0)
+        {
+            Debug.Log("Shooting with " + weaponName + ". Ammo left: " + (ammo - 1));
+            ammo--;
+        }
+        else
+        {
+            Debug.Log("Out of ammo!");
+        }
+    }
+}
+
+[CreateAssetMenu(fileName = "NewSword", menuName = "Weapons/Sword")]
+public class Sword : Weapon
+{
+    // ฟังก์ชันโจมตีเฉพาะสำหรับดาบ
+    public override void Attack()
+    {
+        Debug.Log("Swinging the sword: " + weaponName);
+    }
+}
